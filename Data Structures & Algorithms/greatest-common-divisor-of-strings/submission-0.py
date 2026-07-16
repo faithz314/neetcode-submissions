ad696ddec9511c3c987e785cbae1f9e4d1bb0b1c
faith <138264124+faithz314@users.noreply.github.com>
2026-07-15 21:45:20 -0700

    Add: greatest-common-divisor-of-strings - submission-0

---
+++ Data Structures & Algorithms/greatest-common-divisor-of-strings/submission-0.py
@@ -0,0 +1,14 @@
+from math import gcd
+
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        # two strings must share the same repeating base in order to even be considered for gcd
+        # example that doesn't work: "ABCD" and "ABAB" since you have to have s = t+t+t..+t
+
+        if str1 + str2 != str2 + str1:
+            return ""
+
+        # convert each string to a number and find gcd between both numbers
+        length = gcd(len(str1), len(str2))
+
+        return str1[:length]
